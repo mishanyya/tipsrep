7da24f7aa2d1ee7e0c9ea9f924caafbea065c323
mishanyya <mishanyakashin@gmail.com>
2020-04-23 08:21:55 +0400

    добавил яндекс метрику в header.php 230420

--- wp-content/themes/template/header.php
+++ wp-content/themes/template/header.php
@@ -17,7 +17,20 @@
 <meta name='wmail-verification' content='506545f4e5239fad6d0c641bfab98a98' />
 <meta name="msvalidate.01" content="351CDD8671A68F8F2A8EB97EF595F634" />
 <link rel="icon" href="http://addscripts.ru/favicon.ico" type="image/x-icon">
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
+   ym(61142353, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/61142353" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 </head>
 <body class="">
 
