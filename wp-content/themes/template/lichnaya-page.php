20b67e3398cd9fe856b00ba76b282b3ce51fdaa4
mishanyya <mishanyakashin@gmail.com>
2020-11-15 09:55:39 +0400

    добавил образец файла для личной страницы 15 ноября 09.55

---
+++ wp-content/themes/template/lichnaya-page.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @subpackage template
+ Template Name:личная страница
+ */
+get_header(); ?>
+</div><!--закрытие тега container-->
+
+<div class="container"><!--начало основного container of body-->
+<div class="d-none d-xl-block"><!--начало блока для больших экранов,это экран ноутбука или компьютера-->
+  <div class="row"><!--начало основного row of body-->
+    <div class="col-xl-9 bg-light"><!--начало основной части без сайдбара-->
+     <div class="row"><!--начало основной части row-->
+      <div class="col-xl-12"><!--начало основной части div col--12-->
+
+
+          <?php if ( have_posts() ) while ( have_posts() ) : the_post();  ?>
+          <h1 class="text-center"><?php the_title(); ?></h1>
+          <p>
+            <?php the_content(); ?>
+          </p>
+
+          <?php
+          //здесь и далее можно прописать свой код на php
+          //можно дописать код на javascript
+          ?>
+
+          <?php endwhile; ?>
+
+
+        </div>
+      </div>
+
+    </div>
+
+      <?php get_sidebar();  ?>
+
+
+  </div>
+</div><!--конец блока для средних и больших экранов-->
+
+<div class="d-block d-xl-none"><!--начало блока для маленьких экранов,это экран мобильника или планшета-->
+     <div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 bg-light">
+
+
+          <?php if ( have_posts() ) while ( have_posts() ) : the_post();  ?>
+            <?php //вывод содержимого страницы
+            the_content();
+            ?>
+
+          <?php endwhile; ?>
+
+
+
+    </div>
+
+
+
+  </div>
+</div>
+
+</div>
+<?php get_footer();  ?>
--- wp-content/themes/template/sidebar.php
+++ wp-content/themes/template/sidebar.php
@@ -36,9 +36,11 @@ wp_nav_menu( array(
 ?>
 
 <div class="fixed-bottom position-relative border-top">
-	<h4 class="text-center text-secondary">Наш партнер:</h4>
+<!--	<h4 class="text-center text-secondary">Наш партнер:</h4>-->
 <!-- Партнер volgaplastic-a.ru -->
+<!--
 <a href="http://volgaplastic-a.ru/"   class="d-flex justify-content-center"><img src="/wp-content/themes/template/partnerssymbols/lodka.png" alt="изготовление лодок в Астрахани" title="Производство пластиковых лодок в Астрахани" height="31" width="88"></a>
+-->
 <!-- /Партнер volgaplastic-a.ru -->
 </div>
 
